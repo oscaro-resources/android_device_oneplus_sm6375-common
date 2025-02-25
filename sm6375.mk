9451f57947f81d779aa5ddb224c13cb152336a5d
John Galt <johngaltfirstrun@gmail.com>
2025-02-25 18:01:25 -0500

    sm6375-common: Suppress spammy log tags
    Change-Id: If903749a07284551a4e97a7927b02a1a
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>
    Signed-off-by: NoCache-69 <chethangowdasj86@gmail.com>

--- sm6375.mk
+++ sm6375.mk
@@ -315,6 +315,57 @@ PRODUCT_PACKAGES += \
 
 $(call inherit-product, hardware/oplus/oplus-fwk/oplus-fwk.mk)
 
+# Spammy log tags - silence on user/userdebug builds
+SPAMMY_LOG_TAGS := \
+    Diag_Lib \
+    artd \
+    ArtService \
+    SDM \
+    SRE \
+    libsensor-boledalgo \
+    libsensor-parseRGB \
+    libsensor-qshcalapi \
+    sensors \
+    sensors-hal \
+    a2dp_offload \
+    wpa_supplicant \
+    InetDiagMessage \
+    QtiCarrierConfigHelper \
+    DeviceStatisticsService \
+    DeviceInfoHidlClient \
+    NearbyDiscovery \
+    NearbyPresence \
+    NearbySharing \
+    GmsClient \
+    vendor.qti.hardware.display.composer-service \
+    vendor.qti.camera.provider-service_64 \
+    vendor.hardware.vibratorfeature \
+    CamX \
+    CAM_Thumbnail \
+    CAM_CaptureRequestBuilder \
+    CameraLatencyHistogram \
+    ChiX \
+    CHIIQUTILS \
+    CHISEG \
+    CHIUSECASE \
+    DEBUG-portrait_repair \
+    android.hardware.power-service.lineage-libperfmgr \
+    libperfmgr \
+    android.hardware.audio.service \
+    android.hardware.light-V2-ndk.vendor \
+    vibratorfeature-wrapper \
+    android.hardware.vibrator-V1-ndk_platform.vendor \
+    libqti-perfd-client \
+    BATTERY_CHG \
+    BackgroundInstallControlService \
+    BackupTransportManager \
+    ActivityManager
+
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_VENDOR_PROPERTIES += \
+    $(foreach tag,$(SPAMMY_LOG_TAGS),persist.log.tag.$(tag)=S)
+endif
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal

cc4e1cfafea4271c1c3d59f2190460505a41e6f7
nullptr03 <nullptr03@singkolab.my.id>
2025-09-09 02:56:25 +0530

    sm6375-common: Compile HWUI for performance * Activates a few optimizing compiler flags for HWUI. * -fno-omit-frame-pointer, -marm, -mapcs
    Signed-off-by: NoCache-69 <chethangowdasj86@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -72,6 +72,9 @@ DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest_odm.xml
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 3
 BOARD_KERNEL_BASE := 0x00000000

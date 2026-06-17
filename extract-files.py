56a3ebb9925c4906700ecf69a3266c2556724fc4
ralph950412 <ralph950412@gmail.com>
2026-06-18 04:16:44 +0800

    sm6375-common: Patch libwfdcommonutils to remove its libheif dependency
    Due to https://android.googlesource.com/platform/frameworks/av/+/baa3574157c3a90e721da7460b65bb8c6d130158
    
    Change-Id: Ie0105c391ae187b79ec5f5beb150dc786df95d1b

--- extract-files.py
+++ extract-files.py
@@ -56,6 +56,8 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('/my_product', '/product'),
     ('vendor/etc/media_blair/video_system_specs.json', 'vendor/etc/media_holi/video_system_specs.json'): blob_fixup()
         .regex_replace('"max_retry_alloc_output_timeout": 10000,', '"max_retry_alloc_output_timeout": 0,'),
+    'system_ext/lib64/libwfdcommonutils.so': blob_fixup()
+        .remove_needed('libheif.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libinput_shim.so'),
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()

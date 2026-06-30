d45349f42bd5ba3eb3b8a3ac6c8ab504ca84f44b
binbin323 <bin@dhao.cc>
2026-06-30 03:15:50 +0000

    sm6375-common: Patch wfdservice with libaudiobase Change-Id: I8d018184154137f55c8a486b64638225b40db2d1

--- extract-files.py
+++ extract-files.py
@@ -62,6 +62,7 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libaudiobase.so')
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .add_needed('libaudiobase.so')
         .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     (
         'vendor/lib64/libdpps.so',
